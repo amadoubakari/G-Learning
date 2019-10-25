0431c16acbd1b69c056ba97e99400ab65b397a0b
AMADOU BAKARI <amadou_bakari@yahoo.fr>
2019-10-25 16:51:34 +0100

    Pause: home

--- app/src/main/java/com/flys/architecture/custom/CoreState.java
+++ app/src/main/java/com/flys/architecture/custom/CoreState.java
@@ -1,37 +1,38 @@
 package com.flys.architecture.custom;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.flys.architecture.core.MenuItemState;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.flys.fragments.state.HomeFragmentState;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
 // todo : ajouter ici les sous-classes de [CoreState]
-/*@JsonSubTypes({
-  @JsonSubTypes.Type(value = Class1.class),
-  @JsonSubTypes.Type(value = Class2.class)}
-)*/
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = HomeFragmentState.class)}
+)
 public class CoreState {
-  // fragment visité ou non
-  protected boolean hasBeenVisited = false;
-  // état de l'éventuel menu du fragment
-  protected MenuItemState[] menuOptionsState;
+    // fragment visité ou non
+    protected boolean hasBeenVisited = false;
+    // état de l'éventuel menu du fragment
+    protected MenuItemState[] menuOptionsState;
 
-  // getters et setters
+    // getters et setters
 
-  public boolean getHasBeenVisited() {
-    return hasBeenVisited;
-  }
+    public boolean getHasBeenVisited() {
+        return hasBeenVisited;
+    }
 
-  public void setHasBeenVisited(boolean hasBeenVisited) {
-    this.hasBeenVisited = hasBeenVisited;
-  }
+    public void setHasBeenVisited(boolean hasBeenVisited) {
+        this.hasBeenVisited = hasBeenVisited;
+    }
 
-  public MenuItemState[] getMenuOptionsState() {
-    return menuOptionsState;
-  }
+    public MenuItemState[] getMenuOptionsState() {
+        return menuOptionsState;
+    }
 
-  public void setMenuOptionsState(MenuItemState[] menuOptionsState) {
-    this.menuOptionsState = menuOptionsState;
-  }
+    public void setMenuOptionsState(MenuItemState[] menuOptionsState) {
+        this.menuOptionsState = menuOptionsState;
+    }
 }
---
+++ app/src/main/java/com/flys/fragments/state/HomeFragmentState.java
@@ -0,0 +1,6 @@
+package com.flys.fragments.state;
+
+import com.flys.architecture.custom.CoreState;
+
+public class HomeFragmentState extends CoreState {
+}
