89526d429b39cebe0c17877ec7524d3b0020fcb8
AMADOU BAKARI <amadou_bakari@yahoo.fr>
2019-11-26 17:42:01 +0100

    Pause

--- app/src/main/java/com/flys/fragments/behavior/HomeFragment.java
+++ app/src/main/java/com/flys/fragments/behavior/HomeFragment.java
@@ -14,7 +14,7 @@ import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.OptionsMenu;
 
 
-@EFragment(R.layout.fragment_knife_layout)
+@EFragment(R.layout.fragment_shoe_layout)
 @OptionsMenu(R.menu.menu_home)
 public class HomeFragment extends AbstractFragment {
 
